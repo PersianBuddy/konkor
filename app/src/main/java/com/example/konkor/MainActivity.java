88622caef0c415fd535d13d338b9208982a60ceb
PersianBuddy <mnajafi0014@gmail.com>
2020-11-11 10:54:16 +0330

    Add LoginActivity layout

--- app/src/main/java/com/example/konkor/MainActivity.java
+++ app/src/main/java/com/example/konkor/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.konkor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtStart = findViewById(R.id.txtStart);
+
+        txtStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
