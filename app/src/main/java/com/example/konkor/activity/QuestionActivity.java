4319fb40c68628762263777aa09403cddf67cc55
PersianBuddy <mnajafi0014@gmail.com>
2020-11-19 10:09:46 +0330

    Add new functionality to RVQuestionNumberAdapter

--- app/src/main/java/com/example/konkor/activity/QuestionActivity.java
+++ app/src/main/java/com/example/konkor/activity/QuestionActivity.java
@@ -7,8 +7,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.Toast;
+import android.widget.TextView;
 
 import com.example.konkor.R;
 import com.example.konkor.helper.RVQuestionNumberAdapter;
@@ -17,7 +16,6 @@ public class QuestionActivity extends AppCompatActivity {
     private RVQuestionNumberAdapter adapter;
     private String[] questionNumbers;
     RecyclerView rvQuestionNumbers;
-    public static int questionIndex = -1;
     private Button btnSubmit;
 
     @Override
@@ -31,12 +29,36 @@ public class QuestionActivity extends AppCompatActivity {
         adapter = new RVQuestionNumberAdapter(this, questionNumbers);
 
         rvQuestionNumbers.setAdapter(adapter);
-        rvQuestionNumbers.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false));
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL,false);
+        rvQuestionNumbers.setLayoutManager(layoutManager);
 
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(QuestionActivity.this, String.valueOf(questionIndex), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(QuestionActivity.this, String.valueOf(questionIndex), Toast.LENGTH_SHORT).show();
+                adapter.changeAnswerStatus(15, true);
+                adapter.notifyItemChanged(15);
+//                layoutManager.scrollToPosition(15);
+//                adapter.notifyDataSetChanged();
+//                rvQuestionNumbers.postDelayed(() -> {
+//
+//                    if(rvQuestionNumbers.findViewHolderForAdapterPosition(15)!=null )
+//                    {
+//                        adapter.changeAnswerStatus(15, true);
+//                        adapter.notifyItemChanged(15);
+////                        View view =  rvQuestionNumbers.findViewHolderForAdapterPosition(15).itemView;
+////                         TextView textView=view.findViewById(R.id.txt_question_number_item);
+////                         textView.setText("");
+////                         textView.setBackground(getResources().getDrawable(R.drawable.ic_done));
+//
+////                        adapter.setSelectedItemIndex(15);
+////                         View view =  rvQuestionNumbers.findViewHolderForAdapterPosition(15).itemView;
+////                         TextView textView=view.findViewById(R.id.txt_question_number_item);
+////                         textView.setBackground(getResources().getDrawable(R.drawable.solid_circle));
+////                         textView.setTextColor(getResources().getColor(R.color.white));
+//                    }
+//                },50);
+
             }
         });
 
--- app/src/main/java/com/example/konkor/helper/RVQuestionNumberAdapter.java
+++ app/src/main/java/com/example/konkor/helper/RVQuestionNumberAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,20 +12,24 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.konkor.R;
 import com.example.konkor.activity.QuestionActivity;
 
+import java.util.ArrayList;
+
 public class RVQuestionNumberAdapter extends RecyclerView.Adapter<RVQuestionNumberAdapter.ViewHolder> {
     private Context mContext;
     private String [] mData;
-    private int row_index = -1;
+    private int selectedItemIndex = 0;
+    private int[] answerStatus;//1 for answered 0 for not answered
 
 
     public RVQuestionNumberAdapter(Context context, String[] data) {
         mContext = context;
         mData= data;
+        answerStatus = new int [data.length];
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType){
         LayoutInflater inflater = LayoutInflater.from(mContext);
         View view = inflater.inflate(R.layout.rv_question_number_item,parent,false);
         return new ViewHolder(view);
@@ -38,19 +41,27 @@ public class RVQuestionNumberAdapter extends RecyclerView.Adapter<RVQuestionNumb
         if (position ==0){
             holder.txtQuestionNumber.setTextColor(mContext.getResources().getColor(R.color.white));
             holder.txtQuestionNumber.setBackground(mContext.getResources().getDrawable(R.drawable.solid_circle));
-            QuestionActivity.questionIndex = 0;
         }
         holder.txtQuestionNumber.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                row_index = position;
+                selectedItemIndex = position;
                 notifyDataSetChanged();
             }
         });
-        if(row_index==position){
+        if(answerStatus[position]== 1){//if question is answered
+            holder.txtQuestionNumber.setText("");
+            holder.txtQuestionNumber.setBackground(mContext.getResources().getDrawable(R.drawable.ic_done));
+            if (selectedItemIndex == position){//if it's the selected one
+                holder.txtQuestionNumber.setBackground(mContext.getResources().getDrawable(R.drawable.solid_circle));
+                holder.txtQuestionNumber.setTextColor(mContext.getResources().getColor(R.color.white));
+                holder.txtQuestionNumber.setText(mData[position]);
+            }
+            return;
+        }
+        if(selectedItemIndex == position){
             holder.txtQuestionNumber.setTextColor(mContext.getResources().getColor(R.color.white));
             holder.txtQuestionNumber.setBackground(mContext.getResources().getDrawable(R.drawable.solid_circle));
-            QuestionActivity.questionIndex = row_index;
         }else{
             holder.txtQuestionNumber.setTextColor(mContext.getResources().getColor(R.color.black));
             holder.txtQuestionNumber.setBackgroundResource(0);
@@ -68,6 +79,22 @@ public class RVQuestionNumberAdapter extends RecyclerView.Adapter<RVQuestionNumb
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             txtQuestionNumber = itemView.findViewById(R.id.txt_question_number_item);
+        }
+    }
+
+    public int getSelectedItemIndex() {
+        return selectedItemIndex;
+    }
+
+    public void setSelectedItemIndex(int selectedItemIndex) {
+        this.selectedItemIndex = selectedItemIndex;
+    }
+
+    public void changeAnswerStatus(int questionIndex, boolean isAnswered){
+        if (isAnswered){
+            answerStatus[questionIndex] = 1;
+        }else {
+            answerStatus[questionIndex] = 0;
         }
     }
 }
